feff74aedb16367de2fec3c105f95ac20a858c3e
louisbernard18 <louis.bernard@epfl.ch>
2024-02-20 14:58:36 +0100

    20/02 finish interface zone

--- src/ch/epfl/chacun/Zone.java
+++ src/ch/epfl/chacun/Zone.java
@@ -19,19 +19,24 @@ public interface Zone {
     public static int localId(int zoneId) {
         return zoneId % 10;
     }
+    
+    public abstract int id();
 
-    public record Forest(int id, Kind kind) {
+    public record Forest(int id, Kind kind) implements Zone {
         public enum Kind {
             PLAIN,
             WITH_MENHIR,
-            WITH_MUSHROOMS
+            WITH_MUSHROOMS;
         }
     }
 
-    public record Meadow(int id, List<Animal> animals, SpecialPower specialPower) {
+    public record Meadow(int id, List<Animal> animals, SpecialPower specialPower) implements Zone {
         public Meadow {
             animals = List.copyOf(animals);
         }
+    }
+
+    public record Lake(int id, int fishCount, Zone.SpecialPower specialPower) implements Zone {
 
     }
 }
