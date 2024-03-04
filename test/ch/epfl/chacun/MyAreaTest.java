1c20ff81ba709b915eb17944d197c2ae1162283d
louisbernard18 <louis.bernard@epfl.ch>
2024-03-04 18:26:17 +0100

    push

--- test/ch/epfl/chacun/MyAreaTest.java
+++ test/ch/epfl/chacun/MyAreaTest.java
@@ -2,13 +2,12 @@ package ch.epfl.chacun;
 
 import org.junit.jupiter.api.Test;
 
-import java.awt.image.AreaAveragingScaleFilter;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import static ch.epfl.chacun.Area.hasMenhir;
+import static ch.epfl.chacun.Area.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MyAreaTest {
@@ -43,5 +42,32 @@ public class MyAreaTest {
     }
 
     @Test
-    void 
+    void areaMushroomCount() {
+        Area area = new Area<>(Set.of(new Zone.Forest(1, Zone.Forest.Kind.WITH_MUSHROOMS)), new ArrayList<PlayerColor>(), 0);
+        Area area2 = new Area<>(Set.of(new Zone.Forest(1, Zone.Forest.Kind.WITH_MUSHROOMS), new Zone.Meadow(2, List.of(), null), new Zone.Forest(3, Zone.Forest.Kind.WITH_MUSHROOMS)), new ArrayList<PlayerColor>(), 0);
+        Area area3 = new Area<>(Set.of(new Zone.Forest(1, Zone.Forest.Kind.WITH_MUSHROOMS), new Zone.Meadow(2, List.of(), null), new Zone.Forest(3, Zone.Forest.Kind.WITH_MUSHROOMS), new Zone.Forest(4, Zone.Forest.Kind.WITH_MUSHROOMS)), new ArrayList<PlayerColor>(), 0);
+        Area area4 = new Area<>(Set.of(new Zone.Forest(1, Zone.Forest.Kind.PLAIN)), new ArrayList<PlayerColor>(), 0);
+
+        assertEquals(1, mushroomGroupCount(area));
+        assertEquals(2, mushroomGroupCount(area2));
+        assertEquals(3, mushroomGroupCount(area3));
+        assertEquals(0, mushroomGroupCount(area4));
+    }
+
+    @Test
+    void areaAnimalCount() {
+        Animal deer = new Animal(87, Animal.Kind.DEER);
+        Area area = new Area<>(Set.of(new Zone.Meadow(1,  List.of(deer), null)), new ArrayList<PlayerColor>(), 0);
+        Area area2 = new Area<>(Set.of(new Zone.Meadow(1,  List.of(deer, new Animal(88, Animal.Kind.TIGER)), null)), new ArrayList<PlayerColor>(), 0);
+        Area area3 = new Area<>(Set.of(new Zone.Meadow(1,  List.of(deer, new Animal(88, Animal.Kind.TIGER), new Animal(89, Animal.Kind.AUROCHS)), null)), new ArrayList<PlayerColor>(), 0);
+
+        assertEquals(Set.of(), animals(area, Set.of(new Animal(87, Animal.Kind.DEER))));
+        assertEquals(Set.of(), animals(area, Set.of(deer)));
+        assertEquals(Set.of(deer), animals(area2, Set.of(new Animal(88, Animal.Kind.TIGER))));
+        assertEquals(Set., animals(area2, Set.of(new Animal(87, Animal.Kind.TIGER), new Animal(88, Animal.Kind.AUROCHS))));
+        //assertEquals(2, animals(area3, Set.of(new Animal(87, Animal.Kind.TIGER), new Animal(88, Animal.Kind.AUROCHS))));
+        //assertEquals(2, animals(area3, Set.of(new Animal(89, Animal.Kind.AUROCHS))));
+
+
+    }
 }
