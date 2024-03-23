8d82d6fcb956a76d3ebbc7e6339b496686e3b344
Julkzein <jules.delforge@epfl.ch>
2024-03-23 15:30:26 +0100

    coms pt1

--- src/ch/epfl/chacun/Board.java
+++ src/ch/epfl/chacun/Board.java
@@ -4,17 +4,38 @@ import ch.epfl.chacun.ZonePartitions.Builder;
 
 import java.util.*;
 
+
+/**
+ * This class represents the board of the game. It is composed of several private and
+ * public attributes that describe the board and its tiles. These attributes are:
+ * a PlacedTile array describing all the tiles that have been placed on the board, an
+ * int array describing the indexes of the placed tiles in the order they have been placed,
+ * a ZonePartitions describing a partition of all the zones on the board, a Set of Animals
+ * describing the currently cancelled animals of the board, a constant REACH describing the
+ * number of tiles separating the center of the board from its edges.
+ *
+ * @Author Louis Bernard (379724)
+ * @Author Jules Delforge (372325)
+ */
 public final class Board {
     private final PlacedTile[] placedTiles;
-    public final int[] index; ///////// attention c private
+    private final int[] index; ///////// attention c private
     private final ZonePartitions partition;
     private final Set<Animal> cancelledAnimals;
     public static final int REACH = 12;
-    private static final int TOTAL_TILE_COUNT = 625;
+    private static final int TOTAL_TILE_COUNT = 625; //fallait pas plutot mettre un truc variabel
 
     public static final Board EMPTY = new Board(new PlacedTile[TOTAL_TILE_COUNT] , new int[0], ZonePartitions.EMPTY, Set.of());
 
 
+    /**
+     * This constructor creates a new Board with the given parameters.
+     *
+     * @param placedTiles the array of placed tiles on the board
+     * @param index the array of indexes of the placed tiles
+     * @param partition the partition of the zones on the board
+     * @param cancelledAnimals the set of cancelled animals on the board
+     */
     private Board(PlacedTile[] placedTiles, int[] index, ZonePartitions partition, Set<Animal> cancelledAnimals) {
         this.placedTiles = placedTiles;
         this.index = index;
@@ -22,6 +43,8 @@ public final class Board {
         this.cancelledAnimals = Collections.unmodifiableSet(cancelledAnimals);
     }
 
+
+    /**
     public Board(Set<Animal> cancelledAnimals,  int[] index,PlacedTile[] placedTiles, ZonePartitions partition ) {
         this.placedTiles = placedTiles;
         this.index = index;
@@ -35,16 +58,37 @@ public final class Board {
         this.partition = partition;
         this.cancelledAnimals = Set.of();
     }
+     */
 
+    /**
+     * This method gives the index of a placed tile at a given position.
+     *
+     * @param pos the position of the tile
+     * @return the index of the tile at the given position
+     */
     private int index(Pos pos) {
         return (pos.x() + REACH) + (REACH + pos.y()) * 25;
     }
 
+    /**
+     * This method returns the tile at the given position.
+     *
+     * @param pos the position of the tile at the given position.
+     * @return the tile at the given position
+     */
     public PlacedTile tileAt(Pos pos) {
         int index = index(pos);
         return index < 0 || index >= placedTiles.length ? null : placedTiles[index];
     }
 
+/**
+     * This method returns the placed tile that has the given id or throws a new
+     * IllegalArgumentException if no such tile exists on the board.
+     *
+     * @param tileId the id of the tile to be found
+     * @throws IllegalArgumentException if no such tile exists on the board
+     * @return the placed tile with given id
+     */
     public PlacedTile tileWithId(int tileId) {
         for (int index : index) {
             if (placedTiles[index].id() == tileId) {
@@ -54,10 +98,20 @@ public final class Board {
         throw new IllegalArgumentException();
     }
 
+    /**
+     * This method returns the set of cancelled animals on the board.
+     *
+     * @return the set of cancelled animals on the board.
+     */
     public Set<Animal> cancelledAnimals() {
         return cancelledAnimals;
     }
 
+    /**
+     * This method returns the set of occupants on the board.
+     *
+     * @return the set of occupants on the board.
+     */
     public Set<Occupant> occupants() {
         Set<Occupant> occupants = new HashSet<>();
         for (PlacedTile placedTile : placedTiles) {
@@ -68,6 +122,15 @@ public final class Board {
         return occupants;
     }
 
+    /**
+     * This method returns the area containing the given forest zone
+     * or throws a new IllegalArgumentException if no such zone belongs
+     * to the board.
+     *
+     * @param forest the forest zone to be found
+     * @throws IllegalArgumentException if no such zone belongs to the board
+     * @return the area containing the given forest zone
+     */
     public Area<Zone.Forest> forestArea(Zone.Forest forest) {
          for (Area<Zone.Forest> forestArea : partition.forests().areas()) {
              if (forestArea.zones().contains(forest)) {
@@ -77,6 +140,15 @@ public final class Board {
          throw new IllegalArgumentException();
     }
 
+    /**
+     * This method returns the area containing the given meadow zone
+     * or throws a new IllegalArgumentException if no such zone belongs
+     * to the board.
+     *
+     * @param meadow the meadow zone to be found
+     * @throws IllegalArgumentException if no such zone belongs to the board
+     * @return the area containing the given meadow zone
+     */
     public Area<Zone.Meadow> meadowArea(Zone.Meadow meadow) {
         for(Area<Zone.Meadow> meadowArea : partition.meadows().areas()) {
             if(meadowArea.zones().contains(meadow)) {
@@ -86,6 +158,16 @@ public final class Board {
         throw new IllegalArgumentException();
     }
 
+
+    /**
+     * This method returns the area containing the given river zone
+     * or throws a new IllegalArgumentException if no such zone belongs
+     * to the board.
+     *
+     * @param riverZone the river zone to be found
+     * @throws IllegalArgumentException if no such zone belongs to the board
+     * @return the area containing the given river zone
+     */
     public Area<Zone.River> riverArea(Zone.River riverZone) {
         for(Area<Zone.River> riverArea : partition.rivers().areas()) {
             if(riverArea.zones().contains(riverZone)) {
@@ -95,6 +177,15 @@ public final class Board {
         throw new IllegalArgumentException();
     }
 
+    /**
+     * This method returns the area containing the given water zone
+     * or throws a new IllegalArgumentException if no such zone belongs
+     * to the board.
+     *
+     * @param waterZone the water zone to be found
+     * @throws IllegalArgumentException if no such zone belongs to the board
+     * @return the area containing the given water zone
+     */
     public Area<Zone.Water> riverSystemArea(Zone.Water waterZone) {
         for(Area<Zone.Water> riverSystemArea : partition.riverSystems().areas()) {
             if(riverSystemArea.zones().contains(waterZone)) {
@@ -104,17 +195,42 @@ public final class Board {
         throw new IllegalArgumentException();
     }
 
+    /**
+     * This method returns a set of all the meadow areas on the board.
+     *
+     * @return the set of all the meadow areas on the board
+     */
     public Set<Area<Zone.Meadow>> meadowAreas() {
         return partition.meadows().areas();
     }
 
+    /**
+     * This method returns a set of all the water zones areas on the board.
+     *
+     * @return the set of all the water zone areas on the board
+     */
     public Set<Area<Zone.Water>> riverSystemAreas() {
         return partition.riverSystems().areas();
     }
 
+    /**
+     * This method returns true if the two given positions are adjacent
+     *
+     * @return true if the two given positions are adjacent
+     */
     private boolean isAdjacent(Pos pos1, Pos pos2) {
         return Math.abs(pos1.x() - pos2.x()) + Math.abs(pos1.y() - pos2.y()) == 1;
     }
+
+    /**
+     * This method returns the adjacent meadow to the given zone under the form of an
+     * area containing the given meadow zone but all the occupants of the complete
+     * meadow without any open connexions.
+     *
+     * @param pos the positions the given zone.
+     * @param meadowZone the given zone.
+     * @return the adjacent Meadow.
+     */
     public Area<Zone.Meadow> adjacentMeadow(Pos pos, Zone.Meadow meadowZone) {
         Set<Zone.Meadow> meadowsInArea = new HashSet<>();
         Set<Zone.Meadow> adjacentMeadows = new HashSet<>();
@@ -141,6 +257,14 @@ public final class Board {
         return new Area<>(adjacentMeadows, occupants, 0);
     }
 
+    /**
+     * This method returns the number of occupants of the given kind
+     * and color on the board
+     *
+     * @param player the given color.
+     * @param occupantKind the given kind.
+     * @return the number of occupants of the given kind and color on the board.
+     */
     public int occupantCount(PlayerColor player, Occupant.Kind occupantKind) {
         int count = 0;
         for (PlacedTile tile : placedTiles) {
@@ -151,6 +275,13 @@ public final class Board {
         return count;
     }
 
+    /**
+     * This method returns the set of insertion positions on the board. (The positions
+     * where a tile can be placed)
+     *
+     *
+     * @return the set of insertion positions on the board.
+     */
     public Set<Pos> insertionPositions() {
         Set<Pos> positions = new HashSet<>();
         for(PlacedTile placedTile : placedTiles) {
@@ -174,6 +305,11 @@ public final class Board {
         return positions;
     }
 
+    /**
+     * This method returns the last tile placed on the board.
+
+     * @return the last tile placed on the board or null if the board is empty.
+     */
     public PlacedTile lastPlacedTile() {
         if (index.length > 0) {
             return placedTiles[index[index.length - 1]];
@@ -181,6 +317,12 @@ public final class Board {
         return null;
     }
 
+    /**
+     * This method returns the set of all the forest areas that were closed by the placement
+     * of the last tile on the board or empty if the board is empty.
+     *
+     * @return the set of all the forest areas that were closed by the placement of the last tile on the board.
+     */
     public Set<Area<Zone.Forest>> forestsClosedByLastTile() {
         Set<Area<Zone.Forest>> closedForests = new HashSet<>();
         PlacedTile lastTile = lastPlacedTile();
@@ -196,6 +338,13 @@ public final class Board {
         return closedForests;
     }
 
+
+    /**
+     * This method returns the set of all the river areas that were closed by the placement
+     * of the last tile on the board or empty if the board is empty.
+     *
+     * @return the set of all the forest river that were closed by the placement of the last tile on the board.
+     */
     public Set<Area<Zone.River>> riversClosedByLastTile() {
         Set<Area<Zone.River>> closedRivers = new HashSet<>();
         PlacedTile lastTile = lastPlacedTile();
@@ -211,6 +360,13 @@ public final class Board {
         return closedRivers;
     }
 
+
+    /**
+     * This method returns if the given PlacedTile can be added to the board. (if its position is an insertion position
+     * and if all the sides of the tile are compatible with the sides of the adjacent tiles)
+     *
+     * @return if the given PlacedTile can be added to the board.
+     */
     public boolean canAddTile(PlacedTile placedTile) {
         if (insertionPositions().contains(placedTile.pos())) {
             PlacedTile tileEast = tileAt(placedTile.pos().neighbor(Direction.E));
@@ -228,6 +384,12 @@ public final class Board {
         return false;
     }
 
+    /**
+     * This method returns if the given tile can be placed on the board. (if its position is an insertion position
+     * and if all the sides of the tile are compatible with the sides of the adjacent tiles for any of its rotations)
+     *
+     * @return if the given tile can be placed on the board.
+     */
     public boolean couldPlaceTile(Tile tile) {
         for (Pos position : insertionPositions()) {
             for (Rotation rot : Rotation.ALL) {
@@ -239,6 +401,14 @@ public final class Board {
         return false;
     }
 
+    /**
+     * This method returns a new board identical to the current one but with the given tile placed on it or
+     * throws a new IllegalArgumentException if the tile cannot be placed on the board and the board isn't empty.
+     *
+     * @param tile the tile to be placed on the board
+     * @throws IllegalArgumentException if the tile cannot be placed on the board and the board isn't empty
+     * @return a new board identical to the current one but with the given tile placed on it
+     */
     public Board withNewTile(PlacedTile tile) {
         Preconditions.checkArgument(index.length == 0 || canAddTile(tile));
         PlacedTile[] newPlacedTiles = Arrays.copyOf(placedTiles, placedTiles.length);
@@ -260,6 +430,15 @@ public final class Board {
         return new Board(newPlacedTiles, newIndex, newPartitions.build(), cancelledAnimals);
     }
 
+    /**
+     * This method returns a new board identical to the current one but with the occupant added or
+     * throws a new IllegalArgumentException if the tile that should contain the occupant is already
+     * occupied.
+     *
+     * @param occupant the occupant to be placed on the board
+     * @throws IllegalArgumentException if the tile that should contain the occupant is alreadyoccupied.
+     * @return a new board identical to the current one but with the occupant added
+     */
     public Board withOccupant(Occupant occupant) {
         int id = Zone.tileId(occupant.zoneId());
         Preconditions.checkArgument(tileWithId(id).occupant() == null);
@@ -278,6 +457,14 @@ public final class Board {
         return new Board(placedTiles1, index, newPartition.build(), cancelledAnimals);
     }
 
+
+    /**
+     * This method returns a new board identical to the current one but with the given occupant removed
+     * from it.
+     *
+     * @param occupant the occupant to be removed from the board
+     * @return a new board identical to the current one but with the occupant removed from it
+     */
     public Board withoutOccupant(Occupant occupant) {
         int id = Zone.tileId(occupant.zoneId());
         PlacedTile tile = tileWithId(id);
@@ -296,6 +483,15 @@ public final class Board {
         return new Board(placedTiles1, index, newZonePartitionsBuilder.build(), cancelledAnimals);
     }
 
+
+    /**
+     * This method returns a new board identical to the current one but without any occupants in the forest area
+     * or river areas given as parameters.
+     *
+     * @param forests the forest areas to be cleared of their occupants
+     * @param rivers the river areas to be cleared of their occupants
+     * @return a new board identical to the current one but with the given animal added to it
+     */
     public Board withoutGatherersOrFishersIn(Set<Area<Zone.Forest>> forests, Set<Area<Zone.River>> rivers) {
         ZonePartitions.Builder<Zone> newBoardZonePartitionsBuilder = new ZonePartitions.Builder<>(partition);
         PlacedTile[] newPlacedTile = placedTiles.clone();
