fe1d7fc739ef98beea7cf1f859596862f468b835
louisbernard18 <louis.bernard@epfl.ch>
2024-04-19 14:53:54 +0200

    Update DecksUI.java

--- src/ch/epfl/chacun/gui/DecksUI.java
+++ src/ch/epfl/chacun/gui/DecksUI.java
@@ -4,6 +4,9 @@ import ch.epfl.chacun.Occupant;
 import ch.epfl.chacun.Tile;
 import javafx.beans.value.ObservableValue;
 import javafx.scene.Node;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 
 import java.util.function.Consumer;
 
@@ -12,7 +15,21 @@ public class DecksUI {
     private DecksUI() {}
 
     public static Node create(ObservableValue<Tile> tile, ObservableValue<Integer> normalCount, ObservableValue<Integer> menhirCount, ObservableValue<String> text, Consumer<Occupant> occupantConsumer) {
-        
+        VBox vbox = new VBox();
+        vbox.getStylesheets().add("decks.css");
 
+        HBox hbox = new HBox();
+        vbox.getChildren().add(hbox);
+
+        StackPane normalStackPane = new StackPane();
+        hbox.getChildren().add(normalStackPane);
+
+        StackPane menhirStackPane = new StackPane();
+        hbox.getChildren().add(menhirStackPane);
+
+        StackPane nextTileStackPane = new StackPane();
+        vbox.getChildren().add(nextTileStackPane);
+
+        
     }
 }
